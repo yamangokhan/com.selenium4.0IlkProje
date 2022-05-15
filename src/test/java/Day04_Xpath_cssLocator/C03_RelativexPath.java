1442e6ba5ff318a1396a83a36bfec5eaaf6485e8
yamangokhan <yamangokhanyaman@gmail.com>
2022-05-15 03:16:38 +0300

    SwitchedToNewTag

--- src/test/java/Day04_Xpath_cssLocator/C03_RelativexPath.java
+++ src/test/java/Day04_Xpath_cssLocator/C03_RelativexPath.java
@@ -1,4 +1,4 @@
-package Day04_;
+package Day04_Xpath_cssLocator;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
--- src/test/java/Day04_Xpath_cssLocator/SelfStudy.java
+++ src/test/java/Day04_Xpath_cssLocator/SelfStudy.java
@@ -1,7 +1,6 @@
-package Day04_;
+package Day04_Xpath_cssLocator;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
